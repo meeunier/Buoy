162a8af39d4bcb1f25a875e1b74c174101837468
meeunier <a@meunier.co>
2018-02-08 13:28:49 -0500

    footer work

--- footer.php
+++ footer.php
@@ -9,13 +9,49 @@
  */
 ?>
 
-<!-- <footer class="footer">
+<footer class="footer">
+  <div class="footer-wrapper">
+    <div class="footer-logo">
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="logo" rel="home">
+        <img class="float-center" src="<?php echo get_template_directory_uri(); ?>/logo-footer.png" alt="Logo" width="50px" height="50px" />
+      </a>
+    </div>
+  </div>
     <div class="footer-container">
         <div class="footer-grid">
-            <?php dynamic_sidebar( 'footer-widgets' ); ?>
+
+          <ul class="footer-about">
+            <h3>About</h3>
+            <li><a href="#">About PaddleGuru Studio</a></li>
+            <li><a href="#">Advertise on PaddleGuru</a></li>
+            <li><a href="#">Meet the team</a></li>
+            <li><a href="#">Contact PaddleGuru</a></li>
+            <li><a href="#">Privacy & terms</a></li>
+            <li><a href="#">Media kit</a></li>
+          </ul>
+          <ul class="footer-about">
+            <h3>Brands</h3>
+            <li><a href="#">Sell products on PaddleGuru</a></li>
+            <li><a href="#">Advertise products/a></li>
+            <li><a href="#">Shop on PaddleGuru</a></li>
+          </ul>
+          <ul class="footer-about">
+            <h3>Paddlers</h3>
+            <li><a href="#">Register for a race</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Go to Dashboard</a></li>
+            <li><a href="#">Contact support</a></li>
+          </ul>
+          <ul class="footer-about">
+            <h3>Organizers</h3>
+            <li><a href="#">Create an event</a></li>
+            <li><a href="#">Advertise an event on PaddleGuru</a></li>
+            <li><a href="#">Contact PaddleGuru Race Service</a></li>
+          </ul>
+
         </div>
     </div>
-</footer> -->
+</footer>
 
 <?php wp_footer(); ?>
 </body>
