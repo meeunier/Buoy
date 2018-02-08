4b81045cd8df1f4d9ef49ea47012273603eb06ad
meeunier <a@meunier.co>
2018-02-08 17:20:15 -0500

    set up button s

--- template-parts/content-min.php
+++ template-parts/content-min.php
@@ -12,11 +12,14 @@
 
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div>
+		<div class="paddlers-list-title">
 		<?php the_title( '<h4 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );?>
 		</div>
 	</header>
-	<div class="entry-content">
+	<div class="paddlers-list-excerpts">
 		<?php winwar_first_sentence( the_excerpt() ); ?>
 	</div>
+	<button class="button">
+	<a href="<?php the_permalink(); ?>">read on</a>
+	</button>
 </article>
